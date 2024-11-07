aad491645ac8a2432e772f96eaa3235ab382f443
Tobias Rupprecht <tobias.rupprecht@tobias.rupprecht-F2Y7WWC2M1>
2024-11-07 10:15:46 +0100

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "github_token" {
+  description = "The GitHub personal access token with repo permissions"
+  type        = string
+  sensitive   = true
+}
+
+variable "repository_name" {
+  description = "The name of the GitHub repository to create"
+  type        = string
+}
+
+variable "description" {
+  description = "A short description of the GitHub repository"
+  type        = string
+  default     = ""
+}
+
+variable "visibility" {
+  description = "The visibility of the repository. Options are 'public' or 'private'"
+  type        = string
+  default     = "private"
+  validation {
+    condition     = contains(["public", "private"], var.visibility)
+    error_message = "Visibility must be either 'public' or 'private'."
+  }
+}
