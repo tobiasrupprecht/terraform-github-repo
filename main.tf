ae411ba46c6047aac52e71e516df87456859e110
Tobias Rupprecht <trupprecht@hashicorp.com>
2024-11-07 14:21:47 +0100

    removing vars in main.tf

--- main.tf
+++ main.tf
@@ -3,30 +3,6 @@ provider "github" {
   token = var.github_token
 }
 
-# Define variables
-variable "github_token" {
-  description = "The GitHub personal access token"
-  type        = string
-  sensitive   = true
-}
-
-variable "repository_name" {
-  description = "The name of the GitHub repository"
-  type        = string
-}
-
-variable "description" {
-  description = "The description of the GitHub repository"
-  type        = string
-  default     = ""
-}
-
-variable "visibility" {
-  description = "The visibility of the GitHub repository (private or public)"
-  type        = string
-  default     = "private"
-}
-
 # Create a new GitHub repository
 resource "github_repository" "repo" {
   name        = var.repository_name
